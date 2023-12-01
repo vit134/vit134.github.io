6390f0d396350cee6dc20f266ab9b876d061e015
viandryushkov <viandryushkov@avito.ru>
2023-12-01 21:56:30 +0300

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -20,14 +20,18 @@ const App: React.FC = () => {
 				<DayOne />
 			</div>
 			<div className={styles['col_right']}>
-			<CodeBlock
-				codeBlockStyle={{bacground: '#0f0f23'}}
-				codeContainerStyle={{bacground: 'red'}}
-				language='ts'
-				text={text}
-				showLineNumbers={false}
-				theme={dracula}
-			/>
+				<CodeBlock
+					customStyle={{background: '#0f0f23'}}
+					codeContainerStyle={{
+						background: '#0f0f23',
+						border: `1px solid var(--greenColor)`,
+						padding: '4px 12px',
+					}}
+					language='ts'
+					text={text}
+					showLineNumbers={false}
+					theme={dracula}
+				/>
 			</div>
 		</main>
 
