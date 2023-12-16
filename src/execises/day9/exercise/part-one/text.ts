5e3421d26e6909538c6c779142fca04cd2863638
viandryushkov <viandryushkov@avito.ru>
2023-12-16 19:21:42 +0300

    day 9 part one

---
+++ src/execises/day9/exercise/part-one/text.ts
@@ -0,0 +1,42 @@
+const text = `const getDiff = (row: number[]) => {
+    let result = [];
+
+    for (let i = 1; i < row.length; i++) {
+        result.push(row[i] - row[i - 1]);
+    }
+
+    return result;
+}
+
+export const calculate = (data: string[]) => {
+    return data.reduce((acc, val) => {
+        const bla = [val.split(' ').map(el => Number(el))];
+
+        let isFinish = false;
+        let i = 0;
+
+        while (!isFinish) {
+            const cur = bla[i];
+
+            if (cur.every(el => el === 0)) {
+                isFinish = true;
+                break;
+            }
+
+            bla.push(getDiff(cur));
+            i++;
+        }
+
+        let plus = 0;
+
+        for (let j = bla.length - 2; j >= 0; j--) {
+            const cur = bla[j];
+
+            plus = cur[cur.length - 1] + plus;
+        }
+
+        return acc + plus;
+    }, 0)
+};`;
+
+export default text;
---
+++ src/execises/day9/exercise/part-two/index.ts
@@ -0,0 +1,8 @@
+// import { path, data } from './mock';
+import { data } from '../data';
+
+export const calculate = (data: string[]) => {
+    
+};
+
+console.log('result', calculate(data))
